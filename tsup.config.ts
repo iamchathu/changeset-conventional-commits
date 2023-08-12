47bd3f835867533a3c2310db2b391096cc73a0f4
Chathu Vishwajith <4evercreations@gmail.com>
2023-08-12 15:40:54 +0200

    chore: :wrench: update tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src', '!src/**/*.spec.*'],
   outDir: 'bin',
+  platform: 'node',
+  treeshake: true,
   splitting: false,
   sourcemap: 'inline',
   clean: true,
   bundle: false,
+  target: 'node16',
   format: 'cjs',
   tsconfig: 'tsconfig.build.json',
 });
